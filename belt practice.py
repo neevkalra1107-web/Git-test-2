007854c8b06b9be39a6b7e2ecba5a1ddd003ec41
_neev11_ <neevkalra1107@gmail.com>
2026-08-15 18:56:28 +0530

    new files added

---
+++ belt practice.py
@@ -0,0 +1,49 @@
+# number=int(input())
+# last_digit=number%10
+# print("Last digit is",last_digit)
+# if number%2==0:
+#     print("Last digit is even")
+# else:
+#     print("last digit is odd")
+# if number>500:
+#     print("Number is greater than 500")
+# else:
+#     print("Number is not greater than 500")
+    
+# #Students marks
+# marks=int(input("Enter your marks: "))
+# attendance=float(input("enter your attendence percentage: "))
+# if marks>=40 and attendance>=75:
+#     print("Pass")
+# else:
+#     print("fail")
+
+#Electricity bill
+# units=float(input("Enter your units consumed: "))
+# bill=0
+# if 0<=units<=100:
+#     bill=units*s
+# elif 101<=units<=200:
+#     bill=units*7
+# else:
+#     bill=units*10
+# print("Bill: ",bill)
+
+# age=int(input("Enter your age: "))
+# if age>=18:
+#     entrance_Exam=input("Passed or not passed the exam?: ")
+#     if entrance_Exam=="Passed":
+#         Documents=input("verified or not verified?: ")
+#         if Documents=="verified":
+#             print("You are eligible for admission")
+#     elif entrance_Exam=="not passed":
+#             print("Entrance exam not cleared")
+#     else:
+#         print("Documents not verified")
+# else:
+#     print("Too young!")
+
+#Debugging
+# 
+a=input()
+print("You entered:",a)
